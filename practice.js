a9c9e5181a1d73e27d194fed74bdb3fc08c60586
enwengx <44639680+enwengx@users.noreply.github.com>
2021-05-31 19:56:25 +0800

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,3 +1,6 @@
+/**
+* 实现函数体
+*/
 var foo = function(...args) { // 要求实现函数体}
 var f1 = foo(1,2,3); f1.getValue(); // 6 输出是参数的和
 var f2 = foo(1)(2,3); f2.getValue(); // 6
@@ -22,32 +25,56 @@ function test() {
   })
 }
 test()
-  
 
-  
+/**
+* 求最终left哈right的宽度
+*/
+<div class="container">
+    <div class="left"></div>
+    <div class="right"></div>
+</div>
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  .container {
+    width: 600px;
+    height: 300px;
+    display: flex;
+  }
+  .left {
+    flex: 1 2 300px;
+    background: red;
+  }
+  .right {
+    flex: 2 1 200px;
+    background: blue;
+  }
+ // 第二种
+  .left {
+    flex: 1 2 500px;
+    background: red;
+  }
+  .right {
+    flex: 2 1 400px;
+    background: blue;
+  }
+</style>
+
+//answer-----------------------------answer
+// 实现函数体
 function foo(...args) {
   const target = (...args1)=> foo(...args,...args1)
   target.getValue = ()=> args.reduce((a,b)=> a + b, 0)
   return target
 }
   
-//async function
+//async function，改造
 for(let x of list) {
     const res = await square(x);
     console.log(res,Date.now())
 }
+// 最终结果333.33 266.67
+
