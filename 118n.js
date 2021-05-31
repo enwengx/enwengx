141c51186340900eb24a6186a780a08ab71ca8fd
enwengx <44639680+enwengx@users.noreply.github.com>
2021-05-31 09:01:31 +0800

    Update 118n.js

--- 118n.js
+++ 118n.js
@@ -1,3 +1,16 @@
+const chineseExp = /('([^']*?[\u4e00-\u9fa5]+[^']*?)'|"([^"]*?[\u4e00-\u9fa5]+[^"]*?)"|>([^<]*?[\u4e00-\u9fa5]+[\s\S]*?)<\/)/g;
+const chineseExpWithAttr = /(\S+\=)('([^']*?[\u4e00-\u9fa5]+[^']*?)'|"([^"]*?[\u4e00-\u9fa5]+[^"]*?)")/g;
+const hasChinese = /[\u4e00-\u9fa5]/g;
+const hasQuote = /`([^`]+)`/g;
+const hasExp = /\$\{[^{^}]+\}/g;
+const hasChineseInQuote = /`([^`]*[\u4e00-\u9fa5]+[^`]*)`/g;
+const endWithChinese = />[^<]*?[\u4e00-\u9fa5]+[^<]*?$/g;
+const closeTag = /^([^<]*?)<\//g;
+const blockCommentStartScript = /\s*?\/\*/g;
+const blockCommentEndScript = /\*\//g;
+const blockCommentStartTemplate = /\s*<\!--/g;
+const blockCommentEndTemplate = /\s*-->/g;
+
 walk(folder){
   const subs = fs.readdirSync(folder);
   subs.forEach(sub=>{
