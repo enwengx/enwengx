702703fe2a8136d9e10bd507d4676ef27e96c9ce
enwengx <44639680+enwengx@users.noreply.github.com>
2021-06-01 12:11:20 +0800

    Update note.js

--- note.js
+++ note.js
@@ -36,3 +36,8 @@
 9.v-if会调用addIfCondition方法，生成vnode的时候会忽略对应节点，render的时候就不会渲染；
   v-show会生成vnode，render的时候也会渲染成真实节点，只是在render过程中会在节点的属性中修改show属性值，也就是常说的display；
   v-html会先移除节点下的所有节点，调用html方法，通过addProp添加innerHTML属性，归根结底还是设置innerHTML为v-html的值
+10. '10000000000'.replace( /\B(?=(?:\d{3})+$)/g, ',' ) '10000000000'.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+  (?=pattern)为例，就是捕获以pattern结尾的内容xxx
+  (?!pattern)为例，就是捕获不以pattern结尾的内容xxx
+  (?<=pattern)xxx为例，就是捕获以pattern开头的内容xxx
+  (?<!pattern)xxx为例，就是捕获不以pattern开头的内容xxx
